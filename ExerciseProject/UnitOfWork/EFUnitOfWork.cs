9b4b774f705bf74549d3b17df0d509a31f31d12c
Soheila Golshan <s_golshan22@yahoo.com>
2021-03-07 05:24:42 -0800

    add unitofwork and repository folders

---
+++ ExerciseProject/UnitOfWork/EFUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using ExerciseProject.DataBaseCon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExerciseProject.UnitOfWork
+{
+    public class EFUnitOfWork : UnitOfWorkRepository
+    {
+        private readonly DataBaseContext _context;
+        public EFUnitOfWork(DataBaseContext dataBaseContext)
+        {
+            _context = dataBaseContext;
+        }
+        public void complete()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
